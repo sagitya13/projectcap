c48ac1ca33dc9c3aa1dfca7642357361787815a8
sagitya13 <79758303+sagitya13@users.noreply.github.com>
2024-03-05 11:52:42 +0530

    main

---
+++ src/class/chat/chat.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, provideZoneChangeDetection } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SchoolService } from 'src/shared/services/school.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+  UserName = '';
+  MessageContent = '';
+  
+  CourseId: number;
+  messages: any[] = [];
+  
+
+  constructor(private school: SchoolService,private route:ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params=>{
+      this.CourseId=+params.get('CourseId');
+      this.getMessages();
+    });
+    
+  }
+
+
+  onSubmit() {
+    this.school.sendMessage(this.UserName, this.MessageContent,this.CourseId).subscribe(
+      response => console.log('Message sent successfully'),
+      error => console.error('Failed to send message')
+    );
+  
+}
+
+getMessages() {
+  console.log (this.CourseId);
+  this.school.getMessagesByCourseId(this.CourseId).subscribe(
+    messages => {
+      this.messages = messages;
+      // this.changeDetectorRef.detectChanges();/
+    },
+    error => console.error('Failed to fetch messages')
+  );
+}
+}
